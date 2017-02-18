b3948deaf9af96b35fac523280c87aa61d90f98d
kamyar <k.ghasemlou@gmail.com>
2017-02-18 21:37:55 +0300

    implm. add new payment form

--- frontend/react/PaymentsInspectionComp.js
+++ frontend/react/PaymentsInspectionComp.js
@@ -16,9 +16,12 @@ export default class PaymentsInspectionComp extends React.Component {
             "CallbackHandler",
             "PromiseHandler",
             "FilterPaymentHandler",
+            "submitNewPayment",
             "AddPaymentHandler",
+            "cancelAddPayment",
             "getPaymentsTableComp",
             "PaymentMethodChangeHandler",
+            "getInputForm",
         ]);
         this.state = {
             feedback: {},
@@ -55,6 +58,16 @@ export default class PaymentsInspectionComp extends React.Component {
 
     AddPaymentHandler() {
         console.log("AddPaymentHandler");
+        this.setState({
+            showInputForm: true,
+        })
+    }
+
+    cancelAddPayment() {
+        console.log("AddPaymentHandler");
+        this.setState({
+            showInputForm: false,
+        })
     }
 
     PaymentMethodChangeHandler(e) {
@@ -62,6 +75,19 @@ export default class PaymentsInspectionComp extends React.Component {
             paymentMethod: e.target.value,
         });
     }
+
+    submitNewPayment(e, v) {
+        e.preventDefault();
+        var doc = {};
+        doc.amount = this.refs.amount.value
+        doc.currency = this.refs.currency.value
+        doc.method = this.refs.method.value
+        doc.merchant = this.refs.merchant.value
+        doc.status = this.refs.status.value
+        console.log(doc);
+        PaymentsAPI.sendNewPayment(doc, (r) => console.log(r), this.ErrorHandler);
+    }
+
 
     getPaymentsTableComp() {
         var PaymentsTableComp;
@@ -105,15 +131,40 @@ export default class PaymentsInspectionComp extends React.Component {
         }
     }
 
-    render() {
+    getInputForm() {
         return (
-            <main>
-                <aside>
-                    <button onClick={this.CallbackHandler}>Callback</button>
-                    <button onClick={this.PromiseHandler}>Promise</button>
-                    <button onClick={this.FilterPaymentHandler}>Filter Payment-Method</button>
-                    <button onClick={this.AddPaymentHandler}>Add payment</button>
-                </aside>
+            <section>
+                <form onSubmit={this.submitNewPayment}>
+                    <input type="number" placeholder="Amount in cents" ref="amount"/>
+                    <select ref="currency">
+                        <option value="USD">USD</option>
+                        <option value="EUR">EUR</option>
+                        <option value="GBP">GBP</option>
+                        <option value="AUD">AUD</option>
+                    </select>
+                    <select ref="method">
+                        <option value="creditcard">Credit Card</option>
+                        <option value="bank-transfer">Bank Transfer</option>
+                        <option value="ideal">iDeal</option>
+                    </select> <br/>
+                    <input type="Merchant" placeholder="Merchant" ref="merchant"/>
+                    <select ref="status">
+                        <option value="accepted">ACCEPTED</option>
+                        <option value="denied">DENIED</option>
+                    </select> <br/>
+                    <button type="reset" onClick={this.cancelAddPayment}> Cancel </button>
+                    <input type="submit" value="Submit" />
+                </form>
+            </section>
+        );
+    }
+
+    render() {
+        var mainComp;
+        if (this.state.showInputForm) {
+            mainComp = this.getInputForm();
+        } else {
+            mainComp = (
                 <section>
                     <span>
                         Payment Method:
@@ -126,6 +177,18 @@ export default class PaymentsInspectionComp extends React.Component {
                     </select>
                     {this.getPaymentsTableComp()}
                 </section>
+            );
+        }
+
+        return (
+            <main>
+                <aside>
+                    <button onClick={this.CallbackHandler}>Callback</button>
+                    <button onClick={this.PromiseHandler}>Promise</button>
+                    <button onClick={this.FilterPaymentHandler}>Filter Payment-Method</button>
+                    <button onClick={this.AddPaymentHandler}>Add payment</button>
+                </aside>
+                {mainComp}
             </main>
         );
     }
