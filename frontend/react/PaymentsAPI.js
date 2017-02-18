180fe9c59109825ed47e1c97403f5b1041915c6a
kamyar <k.ghasemlou@gmail.com>
2017-02-18 20:13:12 +0300

    move request to another method for DRY

--- frontend/react/PaymentsAPI.js
+++ frontend/react/PaymentsAPI.js
@@ -13,6 +13,19 @@ export default class PaymentsAPI {
 		return `${API_ADDR}${path}`
 	}
 
+	static get(path, params, SuccessCB, ErrorCB) {
+		request
+		  .get(this.constructURL('/payments')).query(params)
+		  .end(function(err, resp){
+		  	console.log(resp);
+		    if (err && ErrorCB) {
+		    	ErrorCB(err);
+		    } else if (SuccessCB) {
+		    	SuccessCB(resp.body);
+		    }
+		  });
+	}
+
 	static getHighestPayments(N, SuccessCB, ErrorCB) {
 		var doc = {};
 		doc._sort = 'amount';
@@ -21,25 +34,16 @@ export default class PaymentsAPI {
 			doc._limit = N;
 			doc._page = 1;
 		}
-
-		request
-		  .get(this.constructURL('/payments')).query(doc)
-		  .end(function(err, resp){
-		    console.log('Got data', resp.body);
-		  });
+		this.get(this.constructURL('/payments'), doc, SuccessCB, ErrorCB)
 	}
 
 	static getPaymentsBy(merchant, SuccessCB, ErrorCB) {
+		console.log(SuccessCB, ErrorCB);
 		var doc = {};
 		if (merchant) {
 			doc.merchant = merchant;
 		}
-
-		request
-		  .get(this.constructURL('/payments')).query(doc)
-		  .end(function(err, resp){
-		    console.log('Got data', resp.body);
-		  });
+		this.get(this.constructURL('/payments'), doc, SuccessCB, ErrorCB)
 	}
 
 }
